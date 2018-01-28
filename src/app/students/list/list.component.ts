b38fa8bf17b66f032e9bda970d68d5efcde8ce1a
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-01-28 12:47:11 -0300

    task resolved #4

--- src/app/students/list/list.component.ts
+++ src/app/students/list/list.component.ts
@@ -12,36 +12,16 @@ export class ListComponent  implements OnInit  {
   public students = [];
   public errorMsg;
 
-  public class_th_nome ='';
-  public class_th_status ='';
-  public class_th_mensalidade ='';
-
   showDropDown(): void{
      $('.ui.selection.dropdown').dropdown();
   }
 
-  sortName(): void{
-    switch(this.class_th_nome)
-      {
-          case '':
-            this.class_th_nome = 'sorted ascending';
-            break;
-          case 'sorted ascending':
-            this.class_th_nome = 'sorted descending';
-            break;
-          case 'sorted descending':
-            this.class_th_nome = 'sorted ascending';
-      }
-  }
-
-  
   constructor(private service: StudentsService) { 
       this.service.getAlunos()
       .subscribe( data => this.students = data,
                   error => this.errorMsg = error);
     
   }
-
 
   ngOnInit(){
     {
